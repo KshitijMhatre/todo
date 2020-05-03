7f5b6ebb5083c265f1c0c203c320bd8187166d2d
Kshitij Mhatre <kmhatre@yodlee.com>
2020-05-03 22:53:44 +0530

    Updates

---
+++ precache-manifest.e4ef2120cc31ad88432258f96a0b3db8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe0bf67b9cff8a919c98248cd5730123",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "4f18068ccfcec771196a",
+    "url": "/todo/static/css/main.8bf73b6e.chunk.css"
+  },
+  {
+    "revision": "7f44825a6072d024edb0",
+    "url": "/todo/static/js/2.385944aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.385944aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f18068ccfcec771196a",
+    "url": "/todo/static/js/main.f5089b98.chunk.js"
+  },
+  {
+    "revision": "67114fc4ad6958319bc3",
+    "url": "/todo/static/js/runtime-main.e29500e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.16c7555556908dcab93d629b023118ce.js"
+  "/todo/precache-manifest.e4ef2120cc31ad88432258f96a0b3db8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
